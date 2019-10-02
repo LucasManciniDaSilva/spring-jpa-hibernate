ae2cf16f8cb1f6fd931ad1b2209501395dd4db05
LucasManciniDaSilva <lucasmancini1997@gmail.com>
2019-10-02 15:40:06 -0300

    Create DB and config JPA

--- src/main/java/com/example/course/entities/UserDomain.java
+++ src/main/java/com/example/course/entities/UserDomain.java
@@ -3,28 +3,32 @@ package com.example.course.entities;
 import java.io.Serializable;
 import java.util.UUID;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 
-
 @Data
 @AllArgsConstructor
 @EqualsAndHashCode
 @NoArgsConstructor
 @Builder
-
- public class UserDomain implements Serializable {
-
-
+@Entity
+public class UserDomain implements Serializable {
 	private static final long serialVersionUID = 1L;
-		private UUID id;
-		private String name;
-		private String email;
-		private String phone;
-		private String password;
-		
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private UUID id;
+	private String name;
+	private String email;
+	private String phone;
+	private String password;
 
 }
